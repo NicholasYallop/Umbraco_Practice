222c0ca3a4caa5f5dd09efac12592db57009e389
Nick Yallop <n.yallop@wearewattle.com>
2023-10-16 15:27:39 +0100

    implemented a link hijack

---
+++ Umbraco_Flex/Controllers/ContentNodeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace Umbraco_Flex.Controllers
+{
+	public class ContentNodeController : RenderController
+	{
+		public ContentNodeController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
+		{
+		}
+
+		public IActionResult ContentNodeToHijack()
+		{
+			return Content($"the link to \"{(CurrentPage as ContentNode)?.ContentName ?? "an unknown content node"}\" has been hijacked :)");
+		}
+
+		public override IActionResult Index()
+		{
+			return CurrentTemplate(CurrentPage);
+		}
+	}
+}
