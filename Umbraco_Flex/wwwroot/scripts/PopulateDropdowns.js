e676136d5b651ecc74928fc769e2d73f3680b61f
Nick Yallop <n.yallop@wearewattle.com>
2023-11-10 08:25:42 +0000

    navbar [attempt]
    tried to move navbar to bootstrap
    something in the IOE.css is breaking it, though

--- Umbraco_Flex/wwwroot/scripts/PopulateDropdowns.js
+++ Umbraco_Flex/wwwroot/scripts/PopulateDropdowns.js
@@ -4,16 +4,19 @@ function PopulateDropdowns(data) {
   let all = document.getElementById("all_dropdown");
 
   for (var i = 0; i < data.length; i++) {
+    let li = document.createElement("li");
     let a = document.createElement("a");
     a.href = data[i].url;
     a.innerHTML = data[i].contentName;
+    a.classList.add('dropdown-item');
+    li.appendChild(a);
 
-    all.appendChild(a);
+    all.appendChild(li);
     if (data[i].parent == "News") {
-      news.appendChild(a.cloneNode(true));
+      news.appendChild(li.cloneNode(true));
     }
     else if (data[i].parent == "Blogs") {
-      blogs.appendChild(a.cloneNode(true));
+      blogs.appendChild(li.cloneNode(true));
     }
   }
 }
